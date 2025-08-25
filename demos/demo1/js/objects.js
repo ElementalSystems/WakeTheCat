b49c458308071b593b0a9294240cee0ba5890bc8
Andrew HS <ahigsonsmith@gmail.com>
2025-08-25 11:57:25 +0200

    Demo1 added

---
+++ demos/demo1/js/objects.js
@@ -0,0 +1,54 @@
+import * as THREE from 'three';
+import { textures } from './textures.js';
+
+
+function makeCO(mat, objs, sts = []) {
+    const top = new THREE.Group();
+    const bits = [top];
+    objs.forEach(geo => {
+        var mesh = new THREE.Mesh(geo, mat);
+        mesh.castShadow = true;
+        bits.push(mesh);
+        top.add(mesh);
+    });
+    const controller = new THREE.Object3D();
+    controller.bits = bits; //expose the bits for later substate animations
+    //map out all the states explicitly and add a zero state
+    controller.sts = [{}, ...sts].map(v => ({ x: v.x ?? 0, y: v.y ?? 0, z: v.z ?? 0, rx: v.rx ?? 0, ry: v.ry ?? 0, rz: v.rz ?? 0 }));
+    controller.add(top);
+    return controller;
+}
+
+export const objF = {
+    basket: () => makeCO(
+        new THREE.MeshStandardMaterial({
+            map: textures.frame,
+            side: THREE.DoubleSide,
+            metalness: 0,
+            roughness: 0.1
+        }),
+        [
+            new THREE.CylinderGeometry(4, 4.2, .3, 32).translate(0, -.5, 0),
+            new THREE.TorusGeometry(4, .5, 10, 30).rotateX(3.14 / 2),
+            new THREE.TorusGeometry(4.3, .5, 10, 30, 5.5).rotateX(3.14 / 2).translate(0, .9, 0),
+            new THREE.TorusGeometry(4.6, .5, 10, 30, 5).rotateX(3.14 / 2).translate(0, 1.8, 0).rotateY(-.25),
+            new THREE.TorusGeometry(4.7, .5, 10, 30, 4.8).rotateX(3.14 / 2).translate(0, 2.7, 0).rotateY(-.375)
+        ]),
+    needle: () => makeCO(
+        new THREE.MeshStandardMaterial({
+            color: "#BB8",
+            bumpMap: textures.wood,
+            bumpScale: 1,
+            metalness: 0,
+            roughness: 0.3
+        }),
+        [
+            new THREE.CylinderGeometry(.25, .25, 6, 25),
+            new THREE.CylinderGeometry(.25, .01, 3, 25).translate(0, -4.5, 0),
+            new THREE.CylinderGeometry(.7, .8, 1, 25).translate(0, 3.5, 0),
+        ],
+        [
+            { x: 5 } //state 1 = slide left
+        ]
+    ),
+};
---
+++ demos/demo1/js/util.js
@@ -0,0 +1,71 @@
+
+//general utility code
+
+function setElementClass(e, cls) {
+    if (!e.classList.contains(cls))
+        e.classList.add(cls);
+}
+
+function unsetElementClass(e, cls) {
+    if (e.classList.contains(cls))
+        e.classList.remove(cls);
+}
+
+//a general animation or slow effect utility
+function callEachFrame(time, each, end) {
+    var startTime = 0;
+    var endTime = 0;
+    function func(t) {
+        if (!startTime) {
+            startTime = t;
+            endTime = t + time;
+        }
+        var r = (t - startTime) / (time);
+        if (r < 0) r = 0;
+        if (r > 1) r = 1;
+        each(r);
+        if (r < 1)
+            window.requestAnimationFrame(func);
+        else {
+            if (end) end();
+        }
+    };
+    //call it the first time
+    window.requestAnimationFrame(func);
+}
+
+export function inRange(s, e, v) {
+    if (v < s) return s;
+    if (v > e) return e;
+    return v;
+}
+
+function siso(r) { return 3 * r * r - 2 * r * r * r; }
+function so(r) { return 2 * r - r * r; }
+function si(r) { return r * r; }
+
+function reRange(st, ed, func) {
+    return function (r) {
+        if (r < st) return 0;
+        if (r > ed) return 1;
+        r = (r - st) / (ed - st);
+        if (func) return func(r);
+        return r;
+    }
+}
+
+function inter(r, st, end, func) {
+    if (r < 0) r = 0;
+    if (r > 1) r = 1;
+    if (func) r = func(r);
+    return (r * end) + ((1 - r) * st);
+}
+
+export function tickCount() {
+    return new Date().getTime();
+}
+
+function setMaterialIndex(geo, index) {
+    for (var i = 0; i < geo.faces.length; i += 1)
+        geo.faces[i].materialIndex = index;
+}
